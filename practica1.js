05713adf50cfd61fec81758f41700de8ca4a661f
JuanDiegoSic <diego23jdsc@gmail.com>
2021-07-09 11:16:28 -0500

    i finished te calculator with operations

--- practica1.js
+++ practica1.js
@@ -1,5 +1,10 @@
-// variables globales
 
+
+// variables globales
+let operacion = null;
+let numero1 = 0;
+let numero2 = 0;
+let resultado = 0.0;
 let texto = document.getElementById("texto");
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
@@ -11,7 +16,14 @@ let btn7 = document.getElementById("btn7");
 let btn8 = document.getElementById("btn8");
 let btn9 = document.getElementById("btn9");
 let btn0 = document.getElementById("btn0");
+let btnPunto = document.getElementById("btnPunto");
+let btnReinicio = document.getElementById("btnReinicio");
 let btnSuma = document.getElementById("btnSuma");
+let btnResta = document.getElementById("btnResta");
+let btnMulti = document.getElementById("btnMulti");
+let btnDivision = document.getElementById("btnDivision");
+let btnPotencia = document.getElementById("btnPotencia");
+let btnRaiz = document.getElementById("btnRaiz");
 let btnEnter = document.getElementById("btnEnter");
 
 const fn1 = () => {
@@ -49,17 +61,87 @@ const fn9 = () => {
 const fn0 = () => {
   texto.innerHTML += "0";
 };
+const fnPunto = () => {
+  texto.innerHTML += ".";
+};
 const fnSuma = () => {
-    texto.innerHTML += "+";
-  };
-
-const fnEnter = () => {
-  
-    resultado = parseFloat(texto.textContent)
-    console.log(resultado);
+  numero1 = parseFloat(texto.textContent);
+  texto.innerHTML = "";
+  operacion = "suma";
+};
+const fnResta = () => {
+  numero1 = parseFloat(texto.textContent);
+  texto.innerHTML = "";
+  operacion = "resta";
 };
 
+const fnMulti = () => {
+  numero1 = parseFloat(texto.textContent);
+  texto.innerHTML = "";
+  operacion = "multi";
+};
 
+const fnDivision = () => {
+  numero1 = parseFloat(texto.textContent);
+  texto.innerHTML = "";
+  operacion = "div";
+};
+
+const fnRaiz = () => {
+  numero1 = parseFloat(texto.textContent);
+  texto.innerHTML = "";
+  operacion = "raiz";
+};
+
+const fnPotencia= () => {
+  numero1 = parseFloat(texto.textContent);
+  texto.innerHTML = "";
+  operacion = "potencia";
+};
+
+const fnReinicio = () => {
+  texto.innerHTML = "";
+};
+
+const fnEnter = () => {
+  console.log(texto.textContent);
+  numero2 = parseFloat(texto.textContent);
+  if (operacion == "suma"){
+    resultado = numero1 + numero2;
+    texto.innerHTML = resultado;
+  }
+  else if (operacion == "resta") {
+    resultado = numero1 - numero2;
+    texto.innerHTML = resultado;
+  }
+  else if (operacion == "multi") {
+    resultado = numero1 * numero2;
+    texto.innerHTML = resultado;
+  }
+  else if (operacion == "multi") {
+    resultado = numero1 * numero2;
+    texto.innerHTML = resultado;
+  }
+  else if (operacion == "div") {
+    if (numero2 == 0){
+      texto.innerHTML = "--Syntax Error--";
+    }
+    else{
+      resultado = numero1 / numero2;
+      texto.innerHTML = resultado;
+    }
+  }
+  else if (operacion == "potencia") {
+    resultado = Math.pow(numero1,numero2);
+    texto.innerHTML = resultado;
+  }
+  else if (operacion == "raiz") {
+    resultado = Math.sqrt(numero1);
+    texto.innerHTML = resultado;
+  }
+
+
+};
 
 //Eventos
 btn1.onclick = function () {
@@ -99,14 +181,38 @@ btn9.onclick = function () {
 btn0.onclick = function () {
   fn0();
 };
-
-btnSuma.onclick = function () {
-    fnSuma();
-  };
-
-btnEnter.onclick = function () {
-    console.log(texto.innerHTML);
-  fnEnter();
-  
+btnPunto.onclick = function () {
+  fnPunto();
 };
 
+btnSuma.onclick = function () {
+  fnSuma();
+};
+
+btnResta.onclick = function () {
+  fnResta();
+};
+
+btnMulti.onclick = function () {
+  fnMulti();
+};
+
+btnDivision.onclick = function () {
+  fnDivision();
+};
+
+btnPotencia.onclick = function () {
+  fnPotencia();
+};
+
+btnRaiz.onclick = function () {
+  fnRaiz();
+};
+
+btnReinicio.onclick = function () {
+  fnReinicio();
+};
+
+btnEnter.onclick = function () {
+  fnEnter();
+};
